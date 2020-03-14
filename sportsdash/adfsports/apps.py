950c25458a767565c7d54ac142e23d2184826415
6gvinoth <vinothbalan64@gmail.com>
2020-03-15 00:40:59 +0530

    Added Django base for Dashbord application

---
+++ sportsdash/adfsports/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdfsportsConfig(AppConfig):
+    name = 'adfsports'
---
+++ sportsdash/adfsports/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from adfsports import views
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    # path('', views.adfsports, name='adfsports'),
+    path('', TemplateView.as_view(template_name='base.html')),
+]
